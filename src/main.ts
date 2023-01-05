ac118d513c0569ace1db2b4ed6a399bc6469fefd
HyoBeom <hyobeom@codemind.co.kr>
2023-01-05 16:35:57 +0900

    feat: gql host change

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ import { createApolloProvider } from '@vue/apollo-option'
 const cache = new InMemoryCache()
 const apolloClient = new ApolloClient({
   cache,
-  uri: "http://localhost:30002/graphql",
+  uri: "http://gql.wums.link",
+  // uri: "http://localhost:30002/graphql",
 })
 const apolloProvider = createApolloProvider({
   defaultClient: apolloClient,
